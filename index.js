dba1c46374568e9d488b1763735d58d3d2b48d74
Aman Gaiola <amngairola@gmail.com>
2026-09-04 20:38:51 +0530

    why  and how hoisting

--- index.js
+++ index.js
@@ -20,27 +20,48 @@
             js(); - executes function invocations (calls)
 
 */
+debugger;
+console.log(first); //undefined
+console.log(js1); //js1 full code
+// console.log(a); // not defined
+js1(); //js1 called
 
-console.log(first);
-console.log(js1);
+//let ress = js1();
 
 var first = 10; // first = undifined
 var second = 10;
 
+debugger;
 function js1(a, b) {
   //local variables
-  const sum = a + b;
-  return sum;
+  let name = "this is my name";
+  console.log("js1 called ");
+  console.log(name);
+  return a + b;
 }
+// console.log(name);
+
 function js2(a, b) {
   //local variables
   const sum = a + b;
   return sum;
 }
 
+console.log("this is js4 -> ", js4);
+let js4 = (a, b) => {
+  const sum = a + b;
+  return sum;
+};
+
 //console.log(sum)
-
+debugger;
 var res = js1(first, second);
-// console.log(res);
+console.log(res);
 
+debugger;
 js2();
+
+// var  -> as a variable treat  -> undifined
+// let ->  not hoisted
+// const ->  not hoisted
+// function  -> as a function -> full code save
